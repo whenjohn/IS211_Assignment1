10c309f3264228e9e9b13e3d185edefb397f4640
whenjohn <nguyenjohn@gmail.com>
2019-01-28 00:25:33 -0500

    Cleanted up formatting

--- assignment1_part1.py
+++ assignment1_part1.py
@@ -3,11 +3,11 @@
 """IS 211 - Assignment 1 Part 1"""
 
 def listDivide(numbers=[], divide=2):
-    
+
     count = 0
 
     for num in numbers:
         if (num % divide) == 0:
             count += 1
-    
+
     return count
