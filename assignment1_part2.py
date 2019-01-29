ddbe33d1b05beb1f0a8c239878b17d429348d78b
whenjohn <nguyenjohn@gmail.com>
2019-01-29 14:37:43 -0500

    Added second assignment for Week 1

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -2,11 +2,40 @@
 # -*- coding: utf-8 -*-
 """IS 211 - Assignment 1 Part 2"""
 
-# class Book
 
-#init (author and title)
+class Book(object):
+    """ Object that creates book"""
 
-# func display
-    # print string
+    author = ""
+    title = ""
 
+    def __init__(self, author, title):
+        """Constructor for the Book() class.
 
+        Args:
+            author (string): author of book
+            title (string): title of book
+
+        Attributes:
+            author (string): author of book
+            title (string): title of book
+        """
+        self.author = author
+        self.title = title
+
+    def display(self):
+        """Displays the author and title of a book
+
+        Args:
+            self (unknown): variables from constructor
+
+        Returns:
+            ""No return. Prints value
+
+        Examples:
+
+            >>> book1 = Book("John Steinbek", "Of Mice and Men")
+            book1.display()
+            Of Mice and Men written by author John Steinbek
+        """
+        print (self.title + " written by author " + self.author)
